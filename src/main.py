c5489fc14c882019b2f266cc38e5736475a7cb75
estebanfoucher <73360072+estebanfoucher@users.noreply.github.com>
2025-09-04 16:27:12 +0200

    Video utils (#4)
    * added video writer and reader
    
    * fixed frame_count back

--- src/main.py
+++ src/main.py
@@ -1,36 +1,55 @@
 import os
-import subprocess
-import json
+from video import VideoReader, FFmpegVideoWriter
 
-DATA_FOLDER = "/workspace/data/scene_3/camera_1"
+DATA_FOLDER = "/workspace/data/calibration_intrinsics_1/camera_1"
+
+OUTPUT_FOLDER = "/workspace/output"
 
 def list_videos(folder):
     return [os.path.join(folder, f) for f in os.listdir(folder) if f.endswith(".mp4") or f.endswith(".MP4")]
 
-def get_video_info(video_path):
-    """Get video information using ffmpeg"""
-    try:
-        cmd = [
-            'ffprobe', '-v', 'quiet', '-print_format', 'json',
-            '-show_format', '-show_streams', video_path
-        ]
-        result = subprocess.run(cmd, capture_output=True, text=True, check=True)
-        return json.loads(result.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f"Error getting video info: {e}")
-        return None
+def process_frame(frame):
+    # place holder for processing
+    return frame
 
 def process_video(video_path):
-    pass
+    print(f"Processing {video_path}")
+    reader = VideoReader(video_path)
+    output_path = os.path.join(OUTPUT_FOLDER, os.path.basename(video_path).split(".")[0] + "_output.mp4")   
+    writer = FFmpegVideoWriter(output_path, reader.fps, reader.frame_size)
+    
+    frame_count = reader.frame_count
+    
+    for _ in range(frame_count):
+        ret, frame = reader.read()
+        if not ret:
+            break
+        # place holder for processing
+        frame = process_frame(frame)
+        writer.write(frame)
+    
+    writer.release()
+    
+    # check output video has same size and fps as input video fps upt to 0.1
+    reader_output = VideoReader(output_path)
+    assert abs(reader_output.fps - reader.fps) < 0.1, f"Output video fps {reader_output.fps} is not the same as input video fps {reader.fps}"
+    assert reader_output.frame_size == reader.frame_size, f"Output video frame size {reader_output.frame_size} is not the same as input video frame size {reader.frame_size}"
+    assert reader_output.frame_count == frame_count, f"Output video frame count {reader_output.frame_count} is not the same as input video frame count {frame_count}"
+    reader_output.release()
+    
+    reader.release()
+    
+    print(f"Processed {video_path}")
 
+
+    
 def main():
     video_files = list_videos(DATA_FOLDER)
     print(f"Found {len(video_files)} videos.")
     
     for video_path in video_files:
-        print(f"Processing {video_path}...")
         process_video(video_path)
-        print("-" * 50)
+        
 
 if __name__ == "__main__":
     main()
---
+++ src/video.py
@@ -0,0 +1,51 @@
+import cv2
+import subprocess
+import numpy as np
+
+class FFmpegVideoWriter:
+    def __init__(self, output_path: str, fps: float, frame_size: tuple[int, int]):
+        self.output_path = output_path
+        self.width, self.height = frame_size
+        self.fps = fps
+
+        # Launch ffmpeg process
+        self.process = subprocess.Popen([
+            'ffmpeg',
+            '-y',  # overwrite
+            '-f', 'rawvideo',
+            '-vcodec', 'rawvideo',
+            '-pix_fmt', 'bgr24',
+            '-s', f'{self.width}x{self.height}',
+            '-r', str(self.fps),
+            '-i', '-',  # read from stdin
+            '-an',  # no audio
+            '-c:v', 'libx264',
+            '-pix_fmt', 'yuv420p',
+            self.output_path
+        ], stdin=subprocess.PIPE)
+
+    def write(self, frame: np.ndarray):
+        """Write a single frame"""
+        self.process.stdin.write(frame.tobytes())
+
+    def release(self):
+        self.process.stdin.close()
+        self.process.wait()
+
+class VideoReader:
+    """Video reader using cv2"""
+    def __init__(self, video_path: str):
+        self.video_path = video_path
+        self.cap = cv2.VideoCapture(video_path)
+        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+        self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.frame_size = (self.width, self.height)
+        self.frame_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    
+    def read(self):
+        return self.cap.read()
+    
+    def release(self):
+        self.cap.release()
+        
